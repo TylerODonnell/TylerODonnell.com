9e196583d716a83f61515a6048020231bfedf000
Tyler O'Donnell <tylodonnell@gmail.com>
2016-01-30 22:56:31 -0500

    upgrade gulp

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,22 @@
+
 #!/usr/bin/env bash
 # Codeship Deployment Script
 # Piccirilli Dorsey
 
 # CODESHIP DEPLOY SCRIPT
 # ### Codeship Environment Variables ###
-# ### DEPLOY_PATH       (path on server)
+# ### PROD_DEPLOY_PATH  (production path on server)
+# ### DEV_DEPLOY_PATH   (development path on server)
 # ### USERNAME          (username to login)
 # ### HOST              (ip or hostname)
 # ### PORT              (ssh port number)
+
+DEPLOY_PATH=$DEV_DEPLOY_PATH
+
+if [ "$CI_BRANCH" == "master" ]
+then
+    DEPLOY_PATH=$PROD_DEPLOY_PATH
+fi
 
 # Vars
 date=`date +%Y-%m-%d:%H:%M:%S`
@@ -15,19 +24,29 @@ release="$DEPLOY_PATH/releases/$date"
 echo "$release"
 
 # Create directory
-echo "Create Directory"
+echo "Creating Directory"
 ssh $USERNAME@$HOST -p $PORT mkdir -p "$release"
 
 # Rsync changes
 echo "Rsync Changes"
-rsync -vzcrSLh -e "ssh -p $PORT" --exclude="deploy.sh" --exclude="node_modules" --exclude="src" --exclude=".git*" \
+rsync -vzcrSLh -e "ssh -p $PORT" --exclude="deploy.sh" --exclude="node_modules" --exclude="/src" --exclude=".git*" \
     ./ $USERNAME@$HOST:"$release"
 
 # Symlink
-echo "Symlink"
+echo "No Downtime deployment"
 ssh $USERNAME@$HOST -p $PORT /bin/bash << EOF
     cd $release
     ln -nfs "$release" "$DEPLOY_PATH/current"
+
+    ## Laravel Stuff ##
+    # Environment File
+    # ln -nfs ../../.env .env;
+
+    # Storage
+    #rm -r $release/storage;
+    #ln -nfs ../../storage storage;
+
+    ## End Laravel Stuff ##
 
     # Clean up old versions
     cd "$DEPLOY_PATH/releases"
